36ca8c737b922f72bba81499aec117d619da4822
Tangjj1996 <tangjj_1996@163.com>
2021-08-28 23:50:10 +0800

    feat: xf-audio package beautifully

--- server-app/src/main.ts
+++ server-app/src/main.ts
@@ -2,14 +2,17 @@ import Koa from "koa";
 import proxyConfig from "./config";
 import fetch from "node-fetch";
 import Router from "koa-router";
+import koaBody from "koa-body";
+import { inspect } from "util";
 
 const app = new Koa();
 const router = new Router();
 
 app.use(async (ctx, next) => {
-    ctx.set("Access-Control-Allow-Origin", "*");
+    ctx.set("Access-Control-Allow-Origin", ctx.req.headers.origin);
     ctx.set("Access-Control-Allow-Headers", "Content-Type, Content-Length");
     ctx.set("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+    ctx.set("Access-Control-Allow-Credentials", "true");
     if (ctx.method === "OPTIONS") {
         ctx.body = 200;
     } else {
@@ -18,25 +21,15 @@ app.use(async (ctx, next) => {
 });
 
 for (let key in proxyConfig) {
-    router.post(key, async (ctx, next) => {
+    router.post(key, koaBody(), async (ctx, next) => {
         console.log("This is occur req.url ", ctx.req.url);
-        console.log("This is occur req.headers.content-type ", ctx.req.headers["content-type"]);
-        const result: string | Buffer = await new Promise((resolve) => {
-            let chunks = [];
-            ctx.req.on("data", (chunk) => {
-                chunks.push(chunk);
-            });
-            ctx.req.on("end", () => {
-                console.log(chunks.join(""), "------------");
-                resolve(chunks.join(""));
-            });
-        });
+        console.log("This is occur req.headers.content-type ", ctx.req.headers["content-type"], "\n");
         const rawFetchRes = await fetch(proxyConfig[key], {
             headers: {
                 Host: "raasr.xfyun.cn",
                 "Content-Type": ctx.req.headers["content-type"],
             },
-            body: result,
+            body: ctx.request.body,
             method: "POST",
         });
         const jsonFetchRes = await rawFetchRes.json();
