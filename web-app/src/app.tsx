a6de2fcd685f3a922e3cac84f49368a393b3815c
Tangjj1996 <tangjj_1996@163.com>
2021-08-29 17:33:37 +0800

    feat: web-app api apply

--- web-app/src/app.tsx
+++ web-app/src/app.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Button } from "@material-ui/core";
-import { prepare, upload } from "@src/api";
+import { getProgress, getResult, merge, prepare, upload } from "@src/api";
 import CryptoJS from "crypto-js";
 import useSyncCallback from "@src/hooks/useSyncCallback";
+import type { FailedResponse, SuccessResponse } from "@root/types/app.config";
 
 const APP_ID = "41ac2892";
 const SECRET_KEY = "476dbac45bca3f32bba334f702e3bc4f";
@@ -45,38 +46,143 @@ const App: React.FC = () => {
     const stateSyncFn = useSyncCallback(async () => {
         const curFileListArr = Array.from(curFileList);
         for (let i = 0; i < curFileListArr.length; i++) {
-            const sliceIdInstance = new SliceIdGenerator();
             const time = Math.floor(Date.now() / 1000);
             const signa = createSign(time);
-            const urlParams = new URLSearchParams({
-                app_id: APP_ID,
-                signa: signa,
-                ts: String(time),
-                file_len: String(curFileList[i].size),
-                file_name: curFileList[i].name,
-                slice_num: String(Math.ceil(curFileList[i].size / FILE_PIECE_SICE)),
-            });
+            const sliceIdInstance = new SliceIdGenerator();
 
-            const res = await prepare({
-                method: "POST",
-                mode: "cors",
-                body: urlParams,
-            });
-            if (res.ok === 0) {
-                const file = curFileList[i];
-                const formData = new FormData();
-                formData.append("app_id", APP_ID);
-                formData.append("signa", signa);
-                formData.append("ts", String(time));
-                formData.append("task_id", res.data);
-                formData.append("slice_id", sliceIdInstance.getNextSliceId());
-                formData.append("content", file);
-
-                await upload({
-                    method: "POST",
-                    mode: "cors",
-                    body: formData,
+            const prepareFn = (): Promise<SuccessResponse | FailedResponse> => {
+                return new Promise(async (resolve, reject) => {
+                    const result = await prepare({
+                        method: "POST",
+                        mode: "cors",
+                        body: new URLSearchParams({
+                            app_id: APP_ID,
+                            signa: signa,
+                            ts: String(time),
+                            file_len: String(curFileList[i].size),
+                            file_name: curFileList[i].name,
+                            slice_num: String(Math.ceil(curFileList[i].size / FILE_PIECE_SICE)),
+                        }),
+                    });
+                    if (result.ok === 0) {
+                        resolve(result);
+                    } else {
+                        reject({ ...result, stack: "prepare" });
+                    }
                 });
+            };
+
+            const upLoadFn = (data: string): Promise<SuccessResponse | FailedResponse> => {
+                return new Promise(async (resolve, reject) => {
+                    let start = 0;
+                    const loopUpload = async (): Promise<SuccessResponse | FailedResponse> => {
+                        const fileLen = curFileListArr[i].size < FILE_PIECE_SICE ? curFileListArr[i].size : FILE_PIECE_SICE;
+                        const end = start + fileLen;
+                        const formData = new FormData();
+                        formData.append("app_id", APP_ID);
+                        formData.append("ts", String(time));
+                        formData.append("signa", createSign(time));
+                        formData.append("task_id", data);
+                        formData.append("slice_id", sliceIdInstance.getNextSliceId());
+                        formData.append("content", curFileListArr[i].slice(start, end));
+
+                        let result = await upload({
+                            method: "POST",
+                            mode: "cors",
+                            body: formData,
+                        });
+
+                        start = end + 1;
+
+                        if (end > FILE_PIECE_SICE) {
+                            return result;
+                        } else {
+                            return await loopUpload();
+                        }
+                    };
+                    const result = await loopUpload();
+                    if (result.ok === 0) {
+                        resolve(result);
+                    } else {
+                        reject({ ...result, stack: "upload" });
+                    }
+                });
+            };
+
+            const mergeFn = (data: string): Promise<SuccessResponse | FailedResponse> => {
+                return new Promise(async (resolve, reject) => {
+                    const result = await merge({
+                        method: "POST",
+                        mode: "cors",
+                        body: new URLSearchParams({
+                            app_id: APP_ID,
+                            signa,
+                            ts: String(time),
+                            task_id: data,
+                        }),
+                    });
+                    if (result.ok === 0) {
+                        resolve(result);
+                    } else {
+                        reject({ ...result, stack: "merge" });
+                    }
+                });
+            };
+
+            const getProgressFn = (data: string): Promise<SuccessResponse | FailedResponse> => {
+                return new Promise(async (resolve, reject) => {
+                    const timer = setInterval(async () => {
+                        const result = await getProgress({
+                            method: "POST",
+                            mode: "cors",
+                            body: new URLSearchParams({
+                                app_id: APP_ID,
+                                signa,
+                                ts: String(time),
+                                task_id: data,
+                            }),
+                        });
+                        if (result.ok === 0) {
+                            if (JSON.parse(result.data)?.status === 9) {
+                                resolve(result);
+                                clearInterval(timer);
+                            }
+                        } else {
+                            clearInterval(timer);
+                            reject({ ...result, stack: "getProgress" });
+                        }
+                    }, 5000);
+                });
+            };
+
+            const getresultFn = (data: string): Promise<SuccessResponse | FailedResponse> => {
+                return new Promise(async (resolve, reject) => {
+                    const result = await getResult({
+                        method: "POST",
+                        mode: "cors",
+                        body: new URLSearchParams({
+                            app_id: APP_ID,
+                            signa,
+                            ts: String(time),
+                            task_id: data,
+                        }),
+                    });
+                    if (result.ok === 0) {
+                        resolve(result);
+                    } else {
+                        reject({ ...result, stack: "getResult" });
+                    }
+                });
+            };
+
+            try {
+                const resultPrepare = await prepareFn();
+                const resultUpload = await upLoadFn(resultPrepare.data);
+                const resultMerge = await mergeFn(resultPrepare.data);
+                const resultGetProgress = await getProgressFn(resultPrepare.data);
+                const resultGetResult = await getresultFn(resultPrepare.data);
+            } catch (_) {
+                console.error(_);
             }
         }
     });
