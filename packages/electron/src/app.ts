aee490b5f839eeef0eaddcebc8b0a2bf270ea209
Tangjj1996 <tangjj_1996@163.com>
2021-09-03 14:35:22 +0800

    feat: structure electron app

---
+++ packages/electron/src/app.ts
@@ -0,0 +1,34 @@
+import { BrowserWindow, app } from "electron";
+import server from "@xf-audio/koa-server";
+import path from "path";
+
+const successRes = {
+    ok: 0,
+};
+
+const createWindow = () => {
+    return new Promise(async (resolve, reject) => {
+        const window = new BrowserWindow({
+            width: 1200,
+            height: 800,
+            webPreferences: {
+                nativeWindowOpen: true,
+            },
+        });
+        try {
+            await window.loadURL("http://localhost:8080");
+            await server();
+            resolve(successRes);
+        } catch (_) {
+            reject(_);
+        }
+    });
+};
+
+app.on("ready", async () => {
+    try {
+        await createWindow();
+    } catch (_) {
+        console.error(_);
+    }
+});
